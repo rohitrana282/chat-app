be13fdecef35ccbd3561f6eb41d8cf38e2b612df
rohitrana282 <rohitrana282001@gmail.com>
2021-03-21 12:41:47 +0530

    initialized firebase app

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDPbEgGTBy1y7daSRrvT80dMdv7BHkSzqg',
+  authDomain: 'chat-web-app-69f69.firebaseapp.com',
+  projectId: 'chat-web-app-69f69',
+  storageBucket: 'chat-web-app-69f69.appspot.com',
+  messagingSenderId: '866547978507',
+  appId: '1:866547978507:web:88527f90f7077859a3920e',
+};
+
+firebase.initializeApp(config);
